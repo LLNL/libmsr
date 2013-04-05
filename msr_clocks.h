75f4dfea034424df9da95d8a268a7f409178774a
Barry Rountree <rountree4@llnl.gov>
2013-04-04 21:13:13 -0700

    cleanup

--- msr_clocks.h
+++ msr_clocks.h
@@ -4,11 +4,7 @@
 void read_aperf(int package, uint64_t *aperf); 
 void read_mperf(int package, uint64_t *mperf); 
 void read_tsc  (int package, uint64_t *tsc); 
-//double get_effective_frequency();
-//TP
-
 double get_effective_frequency(int package);
-
 void dump_clocks();
 
 #endif //MSR_CLOCKS_H
