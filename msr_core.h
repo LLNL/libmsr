9321c41276ea7e0ef8824693f138ffb573d1c6e0
Barry Rountree <rountree4@llnl.gov>
2013-04-04 21:11:30 -0700

    Documentation.

--- msr_core.h
+++ msr_core.h
@@ -12,6 +12,18 @@ enum{
 int init_msr();
 void finalize_msr();
 
+// Depending on their function, MSRs can be addressed at either
+// the socket (aka cpu) or core level, and possibly the hardware
+// thread level.  
+//
+//  read/write_msr reads from core 0.
+//  read/write_msr_all_cores_v uses a vector of values.
+//  write_msr_all_cores writes all cores with a single value.
+//  read/write_msr_single_core contains all of the low-level logic.
+//	The rest of the functions are wrappers that call these
+//	two functions.
+//  
+
 void write_msr(int socket, off_t msr, uint64_t val);
 void write_msr_all_cores(int cpu, off_t msr, uint64_t val);
 void write_msr_all_cores_v(int cpu, off_t msr, uint64_t *val);
