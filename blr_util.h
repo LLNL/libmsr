eb20d4a57110d00be8a454ba8c36072ca2a62646
Barry Rountree <rountree4@llnl.gov>
2013-04-04 20:56:13 -0700

    Cleanup, documentation

--- blr_util.h
+++ blr_util.h
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #ifndef BLR_UTIL_H
 #define BLR_UTIL_H
+
+// Returns stop-start in seconds.
 double ts_delta( struct timeval *start, struct timeval *stop );
+
+// Simple interface to mkstemp.  Returns null on error.
 FILE * safe_mkstemp( const char *filetag );
+
+// Simple interface to getenv when expected value is an int.
 int get_env_int( const char *name, int *val );
-#define max(a,b) ((a)>=(b)?(a):(b))
+
 #endif // BLR_UTIL_H
 
