c4bb1e28372e49a6a3145eb477764ee2d5e209dd
Barry Rountree <rountree4@llnl.gov>
2013-04-04 20:55:49 -0700

    Purging unneded files.

--- examples/rapl-dummy.c
+++
@@ -1,9 +0,0 @@
-#include <mpi.h>
-#include <unistd.h>
-int
-main( int argc, char **argv ){
- MPI_Init(&argc, &argv);
- sleep(2);
- MPI_Finalize();
- return 0;
-}
--- mpi/run.sh
+++
@@ -1,5 +0,0 @@
-for i in `seq -f "%02g" 20`
-do
-	make zin > e2e.100s.${i} 2>&1
-done
-
