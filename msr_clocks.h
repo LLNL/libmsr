a14e1a55fdb137c5a6aaeae18747a43489384e71
Barry <rountree4@llnl.gov>
2013-05-08 15:43:52 -0700

    extern C fixes.

--- msr_clocks.h
+++ msr_clocks.h
@@ -1,10 +1,18 @@
 #ifndef MSR_CLOCKS_H
 #define MSR_CLOCKS_H
 
+#ifdef __cplusplus 
+extern "C" {
+#endif
+
 void read_aperf(int package, uint64_t *aperf); 
 void read_mperf(int package, uint64_t *mperf); 
 void read_tsc  (int package, uint64_t *tsc); 
 double get_effective_frequency(int package);
 void dump_clocks();
+
+#ifdef __cplusplus 
+}
+#endif
 
 #endif //MSR_CLOCKS_H
