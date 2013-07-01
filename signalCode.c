5d8b39125a01816da3107455332c71f66acd0b80
shoga1 <shoga1@venus1.(none)>
2013-07-01 09:45:27 -0700

    Took out commandline options functionality from signalCode.c

--- signalCode.c
+++ signalCode.c
@@ -12,13 +12,8 @@ void saveData()
 {
 	struct itimerval tout_val;
 
-	signal(SIGALRM, sayhello);
-
-	FILE * fp;
-	fp = fopen ("linpack_msrValues.out", "a");
-	// write thermal and rapl reads to file 
-	fclose(fp);
-	//reset the signal
+	signal(SIGALRM, saveData);
+	
 	tout_val.it_interval.tv_sec = 0;
 	tout_val.it_interval.tv_usec = 0;
 	tout_val.it_value.tv_sec = 1;
@@ -27,35 +22,8 @@ void saveData()
 	setitimer(ITIMER_REAL, &tout_val, 0);
 }
 
-int main(int argc, char **argv)
+int main()
 {
-	int flags, opt, seconds, tfind;
-	flags = 0;	
-
-	while ((opt = getopt(argc, argv, "nt:")) != 1)
-	{
-		switch(opt)
-		{
-			case 't':
-				seconds = atoi(optarg);
-				tfind = 1;
-				break;
-			default: 
-				printf("Usage: %s [-t seconds] name\n", argv[0]);
-				exit(EXIT_FAILURE);
-		}
-	}
-	if(optind >= argc)
-	{
-		printf("Expected argument after options\n");
-		exit(EXIT_FAILURE);
-	}
-
-	// Making a new .out file that will erase
-	// any old ones
-	FILE *fp;
-	fp = fopen("linpack_msrValues.out", "w");
-	fclose(fp);
 	struct itimerval tout_val;
 	
 	tout_val.it_interval.tv_sec = 0;
