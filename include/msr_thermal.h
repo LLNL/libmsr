ae37d44e2cfc844baa16c208c67279126d4d276d
shoga <shoga1@llnl.gov>
2014-03-28 20:04:20 -0700

    Moved the MSR_MISC functions etc. to it's own .c and .h files from msr_thermal.c and .h. Moved the clockmod functions etc. from msr_thermal.c and .h to msr_clocks.c and .h.

--- include/msr_thermal.h
+++ include/msr_thermal.h
@@ -1,4 +1,4 @@
-/* msrThermal.h
+/* msr_thermal.h
  *
  * 
 */
@@ -11,46 +11,6 @@ struct msr_temp_target{
 	uint64_t temp_target;	//Read only (probably the TCC Activation Temp)
 };
 
-struct misc_enable{
-	uint64_t raw;
-	int fast_string_enable;			// Read/Write (thread scope)	
-	int auto_TCC_enable;				// Read/Write 
-	int performance_monitoring;			// Read	(thread scope)
-	int branch_trace_storage_unavail;		// Read only (thread scope)
-	int precise_event_based_sampling_unavail;	// Read Only (thread scope)
-	int TM2_enable;					// Read/Write 
-	int enhanced_Intel_SpeedStep_Tech_enable;	// Read/Write (Package scope)
-	int enable_monitor_fsm;				// Read/Write (thread scope)
-	int limit_CPUID_maxval;				// Read/Write (thread scope)
-	int xTPR_message_disable;			// Read/Write (thread scope)
-	int XD_bit_disable;				// Read/Write (thread scope)
-	int turbo_mode_disable;				// Read/Write (package scope)
-};
-
-struct clock_mod{
-	uint64_t raw;
-
-// There is a bit at 0 that can be used for Extended On-Demand Clock Modulation Duty Cycle
-// It is added with the bits 3:1. When used, the granularity of clock modulation duty cycle
-// is increased to 6.25% as opposed to 12.5%
-// To enable this, must have CPUID.06H:EAX[Bit 5] = 1
-// I am not sure how to check that because otherwise bit 0 is reserved
-
-	int duty_cycle;		// 3 binary digits
-				// 0-7 in decimal
-				//
-				// Value	Duty Cycle
-				//   0		 Reserved
-				//   1		 12.5% (default)
-				//   2		 25.0%
-				//   3		 37.5%
-				//   4		 50.0%
-				//   5		 63.5%
-				//   6		 75.0%
-				//   7		 87.5%
-
-	int duty_cycle_enable;	// Read/Write
-};
 
 struct therm_stat{
 	uint64_t raw;
@@ -152,12 +112,6 @@ struct pkg_therm_interrupt{
 
 void dump_msr_temp_target(struct msr_temp_target *s);
 void get_temp_target(int socket, struct msr_temp_target *s);
-void dump_misc_enable(struct misc_enable *s);
-void get_misc_enable(int package, struct misc_enable *s);
-void set_misc_enable(int package, struct misc_enable *s);
-void dump_clock_mod(struct clock_mod *s);
-void get_clock_mod(int socket, int core, struct clock_mod *s);
-void set_clock_mod(int socket, int core, struct clock_mod *s);
 void dump_therm_stat(int socket, int core, struct therm_stat * s);
 void dump_therm_interrupt(int socket, int core, struct therm_interrupt *s);
 void dump_pkg_therm_stat(int package, struct pkg_therm_stat * s);
