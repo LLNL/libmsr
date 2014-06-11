9f1cb18237f51d1fb3c3a2c39cf7f470a7dc855e
Barry Rountree <rountree4@llnl.gov>
2014-06-11 11:43:50 -0700

    Quick commit to give Neha access to turbo.

---
+++ include/msr_turbo.h
@@ -0,0 +1,10 @@
+#ifndef MSR_TURBO_H
+#define MSR_TURBO_H
+void enable_turbo( int cpu );
+void disable_turbo( int cpu );
+void enable_all_turbo( );
+void disable_all_turbo( );
+void dump_turbo( );
+#endif //MSR_TURBO_H
+
+
