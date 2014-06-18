b806e5a314a2891ba04c12dc772171d0aa72fcc5
Tapasya Patki <tpatki@email.arizona.edu>
2014-06-17 20:33:55 -0700

    signalCombined files

---
+++ src/signalCombined.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013, Lawrence Livermore National Security, LLC.  
+ * Produced at the Lawrence Livermore National Laboratory  
+ * Written by Kathleen Shoga, shoga1@llnl.gov.
+ * All rights reserved. 
+ ** 
+ * This file is part of libmsr.
+ * 
+ * libmsr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *  
+ * libmsr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libmsr.  If not, see <http://www.gnu.org/licenses/>. 
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <signal.h>
+#include <unistd.h>
+#include "signalCombined.h"
+
+#ifndef SET_UP
+static struct timeval startTime;
+static int init = 0;
+int stop=0;
+#endif
+
+
+
+void printData(int i)
+{
+	signal(SIGALRM, printData);
+	struct timeval currentTime;
+	if(!init)
+	{
+		init = 1;
+		gettimeofday(&startTime, NULL);
+	}
+	struct itimerval tout_val;
+	
+	struct rapl_data r1,r2;
+	
+
+	gettimeofday(&currentTime, NULL);
+	double timeStamp = (double)(currentTime.tv_sec-startTime.tv_sec)+(currentTime.tv_usec-startTime.tv_usec)/1000000.0;
+	fprintf(stdout, "Timestamp: %3.2lf\n", timeStamp);	
+
+	fprintf(stdout, "Core Temperatures: ");
+	dump_thermal_terse();	
+	fprintf(stdout, "\n");
+
+	read_rapl_data(0,&r1);
+	read_rapl_data(1,&r2);
+	fprintf(stdout, "Power Socket/PKG/DRAM: %d %8.4lf %8.4lf \n", 0, r1.pkg_watts, r1.dram_watts);
+	fprintf(stdout, "Power Socket/PKG/DRAM: %d %8.4lf %8.4lf \n", 1, r2.pkg_watts, r2.dram_watts);
+
+	tout_val.it_interval.tv_sec = 0;
+	tout_val.it_interval.tv_usec = 0;
+	tout_val.it_value.tv_sec = 5;
+	tout_val.it_value.tv_usec = 0;
+	
+	setitimer(ITIMER_REAL, &tout_val, 0);
+	
+}
+
