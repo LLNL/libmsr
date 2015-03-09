17af1113304d731d5aca4febaabba2b6da1a220d
Todd Gamblin <tgamblin@llnl.gov>
2015-03-09 01:06:32 -0700

    cleanup.

--- cmd/msr.c
+++ cmd/msr.c
@@ -6,7 +6,15 @@
 #include <string.h>
 #include <errno.h>
 
-#define CMD_PREFIX "msr-"
+#include <sys/stat.h>
+
+// prefixes with which to search for subcommands, relative to msr command.
+static const char *cmd_search_paths[] = {
+  "./msr-",                   // alongside the msr executable.
+  "../libexec/libmsr/msr-",   // in libexec/libmsr in the install prefix.
+  NULL
+};
+
 
 ///
 /// Print some simple usage information.
@@ -24,27 +32,27 @@ void usage(const char *cmd) {
 
 
 ///
-/// Looks for a subcommand in the same directory as a command.
-///
-/// Example:
-///    subcommand_path("/usr/bin/msr", "msr-", "turbo")
-///
-/// Would return:
-///    /usr/bin/msr-turbo
+/// Looks for a subcommand relative to the command location.
 ///
 const char *subcommand_path(const char *command,
-                            const char *prefix,
-
+                            const char **rel_paths,
                             const char *subcommand) {
-  char *command_path = realpath(command, NULL);
+  char command_path[PATH_MAX];
+  realpath(command, command_path);
   const char *command_dir = dirname(command_path);
 
-  size_t pathlen = strlen(command_dir) + strlen(prefix) + strlen(subcommand) + 2;
-  char *subcommand_path = malloc(sizeof(char) * pathlen);
-  sprintf(subcommand_path, "%s/%s%s", command_dir, prefix, subcommand);
+  const char ** relpath;
+  for (relpath = rel_paths; *relpath; relpath++) {
+    char subcommand_path[PATH_MAX];
+    snprintf(subcommand_path, PATH_MAX, "%s/%s%s", command_dir, *relpath, subcommand);
 
-  free(command_path);
-  return subcommand_path;
+    struct stat st;
+    int err = stat(subcommand_path, &st);
+    if (err != -1 && !S_ISDIR(st.st_mode) && (st.st_mode & S_IXUSR)) {
+      return realpath(subcommand_path, NULL);
+    }
+  }
+  return NULL;
 }
 
 
@@ -53,33 +61,38 @@ const char *subcommand_path(const char *command,
 /// executable, and exec's it.
 ///
 int main(int argc, char **argv) {
-  const char *cmd = argv[0];
   if (argc < 2) {
-    usage(cmd);
+    usage(argv[0]);
   }
+
+  // Figure out where the exe lives.
+  char cmd[PATH_MAX];
+  readlink("/proc/self/exe", cmd, PATH_MAX);
 
   // find the first non-option arg.
   int arg_start;
   for (arg_start=1; arg_start < argc; arg_start++) {
     const char *arg = argv[arg_start];
     if (!strlen(arg)) continue;
-    if (arg[0] == '-') usage(cmd);  // -h prints usage, any other arg is error
+    if (arg[0] == '-') usage(argv[0]);  // -h prints usage, any other arg is error
     break;
   }
 
   // figure out what subcommand we're calling.
   const char *subcommand = argv[1];
-  const char *path = subcommand_path(argv[0], CMD_PREFIX, subcommand);
+  const char *path = subcommand_path(cmd, cmd_search_paths, subcommand);
+
+  // no command found -- bail.
+  if (!path) {
+    fprintf(stderr, "No such command: %s\n", subcommand);
+    usage(argv[0]);
+  }
 
   // exec the subcommand with the rest of the args.
   if (execv(path, &argv[arg_start])) {
-    if (errno == ENOENT) {
-      fprintf(stderr, "No such command: %s\n", subcommand);
-      usage(cmd);
-    } else {
-      fprintf(stderr, "%s.\n", strerror(errno));
-    }
+    fprintf(stderr, "%s.\n", strerror(errno));
     exit(1);
   }
+
   exit(0);
 }
