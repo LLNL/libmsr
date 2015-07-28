100f2f9be6fad0a1019121a17a8f2020a5f9f797
Scott Walker <walker91@llnl.gov>
2015-07-28 14:58:29 -0700

    fix for missing memhdlr files

---
+++ include/memhdlr.h
@@ -0,0 +1,53 @@
+/* 
+ * Copyright (c) 2015, Lawrence Livermore National Security, LLC.  
+ * Produced at the Lawrence Livermore National Laboratory  
+ * Written by Scott Walker, walker91@llnl.gov.
+ * All rights reserved. 
+ * 
+ * This file is part of libmsr.
+ * 
+ * libmsr is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * libmsr is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with libmsr.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+#ifndef LIBMSR_MEM
+#define LIBMSR_MEM
+#include <stdlib.h>
+
+// These functions are for libmsr use only. Use outside of libmsr may cause
+// segfaults or disrupt libmsr functions.
+
+#define MEMHDLR_DEBUG
+#define MEMERR_GENERIC (fprintf(stderr, "%s %s::%d ERROR: could not allocate memory\n", getenv("HOSTNAME"), __FILE__, __LINE__))
+
+enum
+{
+    LIBMSR_FREE, LIBMSR_MALLOC, LIBMSR_CALLOC, LIBMSR_REALLOC, LIBMSR_FINALIZE
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void * libmsr_malloc(size_t size);
+void * libmsr_calloc(size_t num, size_t size);
+void * libmsr_realloc(void * addr, size_t size);
+void * libmsr_free(void * addr);
+void * memhdlr_finalize();
+int memory_handler(void * address, void * oldaddr, int dealloc);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/memhdlr.c
@@ -0,0 +1,169 @@
+/*
+* Copyright (c) 2015, Lawrence Livermore National Security, LLC.  
+* Produced at the Lawrence Livermore National Laboratory  
+* Written by Scott Walker, walker91@llnl.gov.
+* All rights reserved. 
+* 
+* This file is part of libmsr.
+* 
+* libmsr is free software: you can redistribute it and/or modify it under the
+* terms of the GNU Lesser General Public License as published by the Free
+* Software Foundation, either version 3 of the License, or (at your option) any
+* later version.
+* 
+* libmsr is distributed in the hope that it will be useful, but WITHOUT ANY
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+* PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+* details.
+* 
+* You should have received a copy of the GNU Lesser General Public License along
+* with libmsr.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "memhdlr.h"
+
+// These functions are for internal libmsr use only. Use outside of libmsr may 
+// cause segfaults or disruption of libmsr functions.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void * libmsr_malloc(size_t size)
+{
+    void * result = malloc(size);
+#ifdef MEMHDLR_DEBUG
+    fprintf(stderr, "MEMHDLR: allocated new array at %p\n", result);
+#endif
+    if (result == NULL)
+    {
+        MEMERR_GENERIC;
+        exit(-1);
+    }
+    memory_handler(result, NULL, LIBMSR_MALLOC);
+    return result;
+}
+
+void * libmsr_calloc(size_t num, size_t size)
+{
+    void * result = calloc(num, size);
+    if (result == NULL)
+    {
+        MEMERR_GENERIC;
+        exit(-1);
+    }
+    memory_handler(result, NULL, LIBMSR_CALLOC);
+    return result;
+}
+
+void * libmsr_realloc(void * addr, size_t size)
+{
+    void * result = realloc(addr, size);
+    if (result == NULL)
+    {
+        MEMERR_GENERIC;
+        exit(-1);
+    }
+    memory_handler(result, addr, LIBMSR_REALLOC);
+    return result;
+}
+
+void * libmsr_free(void * addr)
+{
+    memory_handler(NULL, addr, LIBMSR_FREE);
+    return NULL;
+}
+
+void * memhdlr_finalize()
+{
+    memory_handler(NULL, NULL, LIBMSR_FINALIZE);
+    return NULL;
+}
+
+// Keeps track of all dynamic arrays allocated in libmsr to make deallocation simple
+int memory_handler(void * address, void * oldaddr, int type)
+{
+    static void ** arrays = NULL;
+    static unsigned last = 0;
+    static unsigned size = 3;
+    int i;
+    switch (type)
+    {
+        case LIBMSR_FINALIZE:
+            for (i = 0; i < last; i++)
+            {
+                if (arrays[i])
+                {
+#ifdef MEMHDLR_DEBUG
+                fprintf(stderr, "MEMHDLR: data at %p has been deallocated\n", arrays[i]);
+#endif
+                    free(arrays[i]);
+                }
+            }
+            if (arrays)
+            {
+                free(arrays);
+            }
+            return 0;
+        case LIBMSR_REALLOC:
+#ifdef MEMHDLR_DEBUG
+            fprintf(stderr, "MEMHDLR: recieved address %p, (realloc %p)\n", address, oldaddr);
+#endif
+            for (i = 0; i < last; i++)
+            {
+                if (arrays[i] == oldaddr)
+                {
+                    arrays[i] = address;
+                }
+            }
+            return 0;
+        case LIBMSR_FREE:
+            for (i = 0; i < last; i++)
+            {
+                if (arrays[i] == oldaddr)
+                {
+                    free(oldaddr);
+                    arrays[i] = NULL;
+                }
+            }
+            return 0;
+        case LIBMSR_CALLOC: case LIBMSR_MALLOC:
+            if (arrays == NULL)
+            {
+                arrays = (void **) malloc(size * sizeof(void *));
+                if (arrays == NULL)
+                {
+                    MEMERR_GENERIC;
+                    exit(-1);
+                }
+            }
+#ifdef MEMHDLR_DEBUG
+            fprintf(stderr, "MEMHDLR: arrays tracker is at %p\n", arrays);
+#endif
+            if (last >= size)
+            {
+                void ** temp = arrays;
+                size *= 2;
+                temp = (void **) realloc(arrays, size * sizeof(void *));
+                if (temp == NULL)
+                {
+                    MEMERR_GENERIC;
+                    exit(-1);
+                }
+                arrays = temp;
+            }
+            if (address)
+            {
+                arrays[last] = address;
+                last++;
+            }
+            return 0;
+    }
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
