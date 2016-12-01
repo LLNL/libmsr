b3be5d67f5baf62e9719f884f6c972b2374f9272
Daniel Ellsworth <dellswor@cs.uoregon.edu>
2016-12-01 10:43:50 -0800

    Merge pull request #13: Added -c switch to powmon; removes stale named semaphores. Fixes #12.
    Signed-off-by: Stephanie Labasan <labasan1@llnl.gov>

--- demoapps/powmon/src/powmon.c
+++ demoapps/powmon/src/powmon.c
@@ -86,7 +86,13 @@ int main(int argc, char **argv)
     if (argc < 2)
     {
         printf("Usage: %s <executable> <args>...\n", argv[0]);
+        printf("       %s -c\n",argv[0]);
         return 1;
+    }
+    if (strncmp("-c", argv[1], 3) == 0)
+    {
+        highlander_clean();
+        return 0;
     }
 
     if (highlander())
